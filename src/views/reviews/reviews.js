4eed2321a81167b812c5153c5ad90d2a2a040d18
rolESolar <roland.kreindl@gmail.com>
2022-09-06 15:02:34 +0200

    Update Styles

--- src/views/reviews/reviews.js
+++ src/views/reviews/reviews.js
@@ -1,17 +1,23 @@
 const ServiceProvider = require("../../../framework/service_provider");
-let location = "";
+let locationNow = "";
 
 module.exports = class Reviews {
   constructor(params) {
     this.dbConnectionService = ServiceProvider.create("dbconnectionService");
     console.log(params.locationNow);
-    location = params.locationNow;
+    locationNow = params.locationNow;
   }
 
   async data() {
     this.dbConnectionService.establishConnection();
-    let restaurants = await this.dbConnectionService.getRestaurants(location);
+    let restaurants = await this.dbConnectionService.getRestaurants(
+      locationNow
+    );
     console.log(restaurants);
-    return { title: "Restaurants", restaurants: restaurants };
+    return {
+      title: "Restaurants",
+      restaurants: restaurants,
+      locationShow: restaurants[0].region,
+    };
   }
 };
