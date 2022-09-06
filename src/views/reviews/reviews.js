d00c91e6f530babe945b8e1b180258cf6b4021b6
Yewstone <106429681+Yewstone@users.noreply.github.com>
2022-09-06 13:01:59 +0200

    Update Layout

--- src/views/reviews/reviews.js
+++ src/views/reviews/reviews.js
@@ -1,35 +1,19 @@
 const ServiceProvider = require("../../../framework/service_provider");
+let location = "";
 
-let resultScore = 0;
+module.exports = class Reviews {
+  constructor(params) {
+    //this.dbConnectionService = ServiceProvider.create("dbconnectionService");
+    console.log(params.locationNow);
+    location = params.locationNow;
+  }
 
-module.exports = class Locations {
-  constructor(params) {}
-  data() {
-    resultScore = getValuesfromDatabase();
-    return { title: "reviews", score: resultScore };
+
+  async data() {
+    //this.dbConnectionService.establishConnection();
+    //let score = await this.dbConnectionService.getValue();
+    //console.log(score);
+    console.log(location);
+    return { title: "reviews", score: "3.5", locationNow: location};
   }
 };
-
-function getValuesfromDatabase() {
-  var mysql = require("mysql");
-
-  var con = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "",
-    database: "yelp2_0",
-  });
-
-  connection.connect();
-  
-  con.connect(function (err) {
-    if (err) throw err;
-    con.query("SELECT score FROM review", function (err, result, fields) {
-      if (err) throw err;
-      console.log(result);
-      resultScore = result;
-    });
-  });
-  
-  return resultScore;
-}
